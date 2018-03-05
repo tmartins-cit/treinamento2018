97de3085635543f68a852e5ee37290b1edc4eb6a
pedro <pedro.catalini@hotmail.com>
2018-03-05 12:01:45 -0300

    Criação de Controller para Pedidos

--- Treinamento.Presentation.Mvc/Controllers/HomeController.cs
+++ Treinamento.Presentation.Mvc/Controllers/HomeController.cs
@@ -10,29 +10,10 @@ namespace Treinamento.Presentation.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        private Treinamento.Business.Interfaces.IPedidos _pedidosBO;
-        public HomeController()
-        {
-            _pedidosBO = Treinamento.Business.Factory.FactoryPedidos.CriarClassePedidos();
-
-        }
+        
         public ActionResult Index()
         {
-            var itens = _pedidosBO.RetornarItens();
-            var pedidosModel = new List<PedidoModel>();
-            itens.ForEach(pedido =>
-            {
-                PedidoModel pedidoModel = new PedidoModel()
-                {
-                    Codigo = pedido.Codigo,
-                    Descricao = pedido.Descricao,
-                    Numero = pedido.Numero,
-                    TipoPedido = Enum.GetName(pedido.TipoPedido.GetType(), pedido.TipoPedido),
-                    Data = pedido.Data.ToString("dd/MM/yyyy")
-                };
-                pedidosModel.Add(pedidoModel);
-            });
-            return View(pedidosModel);
+            return View();
         }
 
         public ActionResult About()
---
+++ Treinamento.Presentation.Mvc/Controllers/PedidoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Treinamento.Presentation.Mvc.Models;
+
+namespace Treinamento.Presentation.Mvc.Controllers
+{
+
+    public class PedidoController : Controller
+    {
+        private Treinamento.Business.Interfaces.IPedidos _pedidosBO;
+        public PedidoController()
+        {
+            _pedidosBO = Treinamento.Business.Factory.FactoryPedidos.CriarClassePedidos();
+        }
+        
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var itens = _pedidosBO.RetornarItens();
+            var pedidosModel = new List<PedidoModel>();
+            itens.ForEach(pedido =>
+            {
+                PedidoModel pedidoModel = new PedidoModel()
+                {
+                    Codigo = pedido.Codigo,
+                    Descricao = pedido.Descricao,
+                    Numero = pedido.Numero,
+                    TipoPedido = Enum.GetName(pedido.TipoPedido.GetType(), pedido.TipoPedido),
+                    Data = pedido.Data.ToString("dd/MM/yyyy")
+                };
+                pedidosModel.Add(pedidoModel);
+            });
+            return View(pedidosModel);
+        }
+    }
+}
